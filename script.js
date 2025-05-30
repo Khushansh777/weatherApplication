efc7239ed67072066ada62c1f649d6afb4aa6851
Khushansh <khushanshbaliyan@gmail.com>
2025-05-30 20:29:32 +0530

    Enhance weather app functionality by adding event listener for city search, updating fetchData to accept dynamic city input, and displaying weather details including temperature, description, wind speed, humidity, and pressure.

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const apiKey =  '2d245e7b37b6cda7e092b72b37a60620';
 // Declaration of variables
 const input = document.querySelector('#city-input');
 const tempContainer =  document.querySelector('#temperature');
+const descriptionContainer = document.querySelector('#description');
 const cityContainer = document.querySelector('#city');
 const windContainer =  document.querySelector('#wind')
 const humidityConstainer = document.querySelector('#humidity');
 const pressureontainer = document.querySelector('#pressure');
+const searchBtn  = document.querySelector('#search-btn');
+const weatherBoxContainer = document.querySelector('.weather-box');
 
 //fetch Data
-async function fetchData(param){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${apiKey}`;
+const fetchData = async (param)=> {
+    const  url = `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${apiKey}`;
   try {
     const response  = await fetch(url);
     if(!response){
         throw new Error(`Response status: ${response.status}`);
     }
-    const responseJson =response.json()
-    console.log(responseJson);
+     const  data = await response.json()   
+    console.log(data);
+    console.log(data.main.temp)
+    return data
   }
   catch(error){
     console.log(`error ${error}`);
@@ -25,5 +30,42 @@ async function fetchData(param){
 
 }
 
-fetchData()
+// hides when nothing is there
+if(input.value.trim() === ''){
+    weatherBoxContainer.style.display =  'none'
+}
+// fetchData()
+
+searchBtn.addEventListener('click', async() => {
+const inputVal = input.value.trim();
+const inputVal2 = input.value;
+if(inputVal == ''){
+    weatherBoxContainer.style.display = 'none'
+    alert('PLs write a Valid City name')
+    return
+}
+try{
+    const data = await fetchData(inputVal);
+    console.log(data)
+    weatherBoxContainer.style.display = 'block';
+    tempContainer.innerHTML = `${fahrenheitToCelsius(data.main.temp)}°C`;
+    cityContainer.innerHTML = data.name;
+    descriptionContainer.innerHTML = data.weather[0].description;
+    windContainer.innerHTML = `${data.wind.speed} kmph`;
+    humidityConstainer.innerHTML = `${data.main.humidity} %`;
+    pressureontainer.innerHTML = data.main.pressure;
+    }
+catch(error){
+    console.log('error', error);
+}
+    })
+
+// convert into Celsius
+function fahrenheitToCelsius(f) {
+  return Math.round(f - 273.15 );
+}
+
+console.log(fahrenheitToCelsius(297.7))
+
+
 
