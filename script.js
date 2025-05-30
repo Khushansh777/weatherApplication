e74676caa6acff28b47104d97cec9ffd803c5aa9
Khushansh <khushanshbaliyan@gmail.com>
2025-05-30 19:26:22 +0530

    Initialize script.js with API key and variable declarations for weather app

---
+++ script.js
@@ -0,0 +1,11 @@
+//API Key
+const apiKey =  'fe7ae47bce952e4512c47d519b782a96';
+// Declaration of variables
+const input = document.querySelector('#city-input');
+const tempContainer =  document.querySelector('#temperature');
+const cityContainer = document.querySelector('#city');
+const windContainer =  document.querySelector('#wind')
+const humidityConstainer = document.querySelector('#humidity');
+const pressureontainer = document.querySelector('#pressure');
+
+
