f5a8baa18c6907949dbbc4cc22fb3faeb898ef10
Khushansh <khushanshbaliyan@gmail.com>
2025-05-30 19:40:29 +0530

    made function for fetching Data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //API Key
-const apiKey =  'fe7ae47bce952e4512c47d519b782a96';
+const apiKey =  '2d245e7b37b6cda7e092b72b37a60620';
 // Declaration of variables
 const input = document.querySelector('#city-input');
 const tempContainer =  document.querySelector('#temperature');
@@ -8,4 +8,22 @@ const windContainer =  document.querySelector('#wind')
 const humidityConstainer = document.querySelector('#humidity');
 const pressureontainer = document.querySelector('#pressure');
 
+//fetch Data
+async function fetchData(param){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${apiKey}`;
+  try {
+    const response  = await fetch(url);
+    if(!response){
+        throw new Error(`Response status: ${response.status}`);
+    }
+    const responseJson =response.json()
+    console.log(responseJson);
+  }
+  catch(error){
+    console.log(`error ${error}`);
+  }
+
+}
+
+fetchData()
 
